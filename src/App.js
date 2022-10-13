37f74017185044ca5450f4dbad4ab07f976f2c65
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-12 20:42:56 -0400

    Create basic navigation elements

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <h1>UWVR</h1>
-    </div>
+    <NavBar />
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../styles/NavBar.css";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
-  )
+    <nav className="navbar">
+      <a>UWVR</a>
+      <ul className="nav-menu">
+        <li className="nav-menu-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="nav-menu-item">
+          <a href="/about">About</a>
+        </li>
+        <li className="nav-menu-item">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
