5c86f37876d20817764aa831746e2bed7c34257a
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-13 21:58:12 -0400

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import "./App.css";
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from "./pages/About";
+import Contact from './pages/Contact'
 
 function App() {
   return (
-    <NavBar />
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const Contact = () => {
+  return (
+    <div>
+        <NavBar />
+        <h1>Contact</h1>
+    </div>
+  )
+}
+
+export default Contact
