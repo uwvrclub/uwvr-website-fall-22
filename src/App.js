0a3b3ac002fcfdb0c682489ab025e9d6f19c429b
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-13 22:00:09 -0400

    Any invalid URL goes to a 404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter , Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import About from "./pages/About";
 import Contact from './pages/Contact'
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   );
