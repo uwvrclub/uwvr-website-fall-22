630d3aab0b677ae714a614be6f32352e9159dbd4
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-24 21:19:31 -0400

    Add Contacts component

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -5,11 +5,25 @@ function Contacts() {
     return (
         <div className="main">
             <div className="contacts-wrapper">
-                <h2>Contact us at</h2>
-                <p>Email: </p>
-                <p>Instagram: </p>
-                <p>Facebook: </p>
-                <p>Discord: </p>
+                <h2>Contact Us At:</h2>
+                <p>
+                    Email:{' '}
+                    <a href="mailto:uwvrclub@gmail.com">uwvrclub@gmail.com</a>
+                </p>
+                <p>
+                    Instagram:{' '}
+                    <a href="https://www.instagram.com/uwvrclub/">@uwvrclub</a>
+                </p>
+                <p>
+                    Facebook:{' '}
+                    <a href="https://www.facebook.com/UWaterlooVR/">
+                        UWVR Club
+                    </a>
+                </p>
+                <p>
+                    Discord:{' '}
+                    <a href="mailto:uwvrclub@gmail.com">uwvrclub@gmail.com</a>
+                </p>
             </div>
         </div>
     )
