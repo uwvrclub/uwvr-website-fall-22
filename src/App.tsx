b79ca9f6c2172c1d715b42b36b620ad4816c50fd
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-27 19:38:30 -0400

    Create /projects page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import NoPage from './pages/NoPage'
+import Projects from './pages/Projects'
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route index element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="contact" element={<Contact />} />
+                <Route path="projects" element={<Projects />} />
                 <Route path="*" element={<NoPage />} />
             </Routes>
         </BrowserRouter>
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import HeroBanner from '../components/HeroBanner'
+import NavBar from '../components/NavBar'
+
+function Projects() {
+    return (
+        <>
+            <NavBar />
+            <HeroBanner title="Projects" />
+
+            <Footer />
+        </>
+    )
+}
+
+export default Projects
