0ac535e1af2180c48bb5fb4c765b8c025121ff95
Taha Zaryab <tahazaryab10@gmail.com>
2022-10-27 19:15:49 -0400

    Format code

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -8,14 +8,16 @@ const NoPage = () => {
     return (
         <div>
             <NavBar />
-            <HeroBanner src="images/404.jpg" title="Page Not Found"/>
-            <div className='no-page-content'>
-              <div className='no-page-content-wrapper'>
-
-              
-              <h2>Sorry, the page you were looking does not exist.</h2>
-              <p>Return back to the <a href="/">home page</a>, or <a href="/contact">contact us</a> if you can't find what you were looking for!</p>
-              </div>
+            <HeroBanner src="images/404.jpg" title="Page Not Found" />
+            <div className="no-page-content">
+                <div className="no-page-content-wrapper">
+                    <h2>Sorry, the page you were looking does not exist.</h2>
+                    <p>
+                        Return back to the <a href="/">home page</a>, or{' '}
+                        <a href="/contact">contact us</a> if you can't find what
+                        you were looking for!
+                    </p>
+                </div>
             </div>
             <Footer />
         </div>
