cf21e51cd3a8c31a36462aeffb0087b6aeda1aa2
Taha Zaryab <tahazaryab10@gmail.com>
2022-11-09 20:14:54 -0500

    Replace home video, add placeholders

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,8 +4,11 @@ import '../styles/Hero.scss'
 const Hero = () => {
     return (
         <div className="hero-container">
-            <video src="/videos/hero-background.mp4" autoPlay loop muted />
-            <h1 style={{ color: 'white' }}>replace w anything</h1>
+            <video src="/videos/home.mp4" autoPlay loop muted />
+            <h1 style={{ color: 'white' }}>UWVR</h1>
+            <h2 style={{ color: 'white' }}>Explore the wondrous world of XR</h2>
+            <a href='/projects'><span>Our Projects</span></a>
+            <a href='/events'><span>Our Events</span></a>
         </div>
     )
 }
