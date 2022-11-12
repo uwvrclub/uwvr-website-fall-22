a87b9de1527a4f6a2ccb940c6c7b4baf9051b97e
Taha Zaryab <tahazaryab10@gmail.com>
2022-11-11 21:51:17 -0500

    Update events data and slider

--- src/data/PastEventsData.tsx
+++ src/data/PastEventsData.tsx
@@ -1,25 +1,37 @@
 export const PastEventsData = [
     {
+        image: 'images/events/vr-and-chill.png',
+        title: 'VR & Chill',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
+    },
+    {
+        image: 'images/events/coopprep.png',
+        title: 'Co-op Prep Event',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
+    },
+    {
+        image: 'images/events/echo3d.jpeg',
+        title: 'EchoAR (Echo3D) Workshop',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
+    },
+    {
+        image: 'images/events/sparkar.jpeg',
+        title: 'Intro to SparkAR workshop',
+        content:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
+    },
+    {
         image: 'images/events/events-placeholder.jpeg',
-        title: 'UWVR x CTRL V',
+        title: 'Intro to Three.js workshop',
         content:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
     },
     {
-        image: 'images/events/events-placeholder.png',
-        title: 'Past Event 2',
-        content:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
-    },
-    {
-        image: 'images/events/events-placeholder.png',
-        title: 'Past Event 3',
-        content:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
-    },
-    {
-        image: 'images/events/events-placeholder.png',
-        title: 'Past Event 4',
+        image: 'images/events/events-placeholder.jpeg',
+        title: '“Breaking into XR Research” Talk',
         content:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc.',
     },
