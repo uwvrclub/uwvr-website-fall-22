f567de9553cba6f3ef478b276f34c70e70eb689f
Taha Zaryab <tahazaryab10@gmail.com>
2022-11-18 21:32:35 -0500

    Fix hrefs

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -13,7 +13,7 @@ function Projects() {
             <div className="projects">
                 <HeroBanner
                     title="Projects"
-                    src="/build/images/banners/projects.jpeg"
+                    src="images/banners/projects.jpeg"
                 />
                 <h2>Current Projects</h2>
 
@@ -21,13 +21,13 @@ function Projects() {
                     <ProjectCard
                         title="Virtual Reality Escape Room"
                         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc."
-                        src="/build/images/projects/3d-escape-rm.jpg"
+                        src="images/projects/3d-escape-rm.jpg"
                         alt=""
                     />
                     <ProjectCard
                         title="3D Interactive Website"
                         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc."
-                        src="/build/images/projects/3js-website.jpeg"
+                        src="images/projects/3js-website.jpeg"
                         alt=""
                     />
                 </div>
@@ -35,13 +35,13 @@ function Projects() {
                     <ProjectCard
                         title="UWVR Talks: Metaverses"
                         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc."
-                        src="/build/images/projects/podcast.png"
+                        src="images/projects/podcast.png"
                         alt=""
                     />
                     <ProjectCard
                         title="UWVR Stickers"
                         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lacus vel facilisis volutpat est. Dignissim enim sit amet venenatis urna cursus eget nunc."
-                        src="/build/images/events/events-placeholder.jpeg"
+                        src="images/events/events-placeholder.jpeg"
                         alt=""
                     />
                 </div>
